56bae83fa579d539471ce240c27422a2cae3a583
FarBb <loveinsemarang@gmail.com>
2021-04-02 20:24:48 +0700

    Update

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master')
+@section('title', 'MyPayroll')
+
+@section('judul-content')
+<div class="section-header">
+  <h1></h1>
+</div>
+@endsection
+
+@section('content')
+    
+<div class="section-body">
+  <h2>Hahahahaha</h2>
+</div>
+@endsection
